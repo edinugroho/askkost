9673d28cafcf6455262880b395f156f633bf9a54
edinugroho <edidwinugroho80@gmail.com>
2022-12-30 14:20:51 +0700

    test: create user factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,11 +2,16 @@
 
 namespace Database\Factories;
 
-use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\User;
 use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
 
 class UserFactory extends Factory
 {
+
+    protected $model = User::class;
+    
     /**
      * Define the model's default state.
      *
@@ -14,11 +19,15 @@ class UserFactory extends Factory
      */
     public function definition()
     {
+        $type = $this->faker->randomElement(['regular' ,'premium']);
+
         return [
             'name' => $this->faker->name(),
+            'username' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => Hash::make('password'),
+            'type' => $type,
+            'credit' => $type == 'regular' ? 20 : 40,
             'remember_token' => Str::random(10),
         ];
     }
