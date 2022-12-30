2729527e1ce1110193b4c52c5fe7f6681cedcbfa
edinugroho <edidwinugroho80@gmail.com>
2022-12-30 19:46:31 +0700

    feat: owner register

---
+++ app/Http/Controllers/OwnerController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Services\OwnerService;
+use App\Http\Requests\OwnerRequest;
+
+class OwnerController extends Controller
+{
+    public $ownerService;
+
+    public function __construct() {
+        $this->ownerService = app(OwnerService::class);
+    }
+
+    public function create(OwnerRequest $request)
+    {
+        $result = [
+            'status' => 200
+        ];
+
+        try {
+            $result['data'] = $this->ownerService->saveUser($request->all());
+        }  catch (Exception $e) {
+            $result = [
+                'status' => 500,
+                'error' => $e->getMessage()
+            ];
+        } 
+
+        return response()->json($result);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,9 @@
 <?php
 
-use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\OwnerController;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,6 +24,8 @@ Route::get('/', function() {
 });
 
 Route::post('/users', [UserController::class, 'create']);
+
+Route::post('/owners', [OwnerController::class, 'create']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
